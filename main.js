784e096cd5e3d2ad00cf1878bb1e2688e030cf25
cykim8811 <cykim8811@users.noreply.github.com>
2020-06-26 16:21:03 +0900

    Directional Light added

--- main.js
+++ main.js
@@ -5,8 +5,25 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+// Adding Light
 var light = new THREE.AmbientLight(0xffffff, 0.2);
 scene.add(light);
+
+var dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
+dirLight.position.set(0, 0, 0);
+dirLight.castShadow = true;
+dirLight.shadow.mapSize.width = 4096;
+dirLight.shadow.mapSize.height = 4096;
+
+dirLight.shadow.camera.left = -100;
+dirLight.shadow.camera.right = 100;
+dirLight.shadow.camera.top = 100;
+dirLight.shadow.camera.bottom = -100;
+
+scene.add(dirLight);
+scene.add(dirLight.target);
+
+
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -16,7 +33,7 @@ scene.add( cube );
 camera.position.z = 5;
 
 function update() {
-    cube.rotation.x += 0.1;
+    cube.rotation.x += 0.005;
     setTimeout(update, 0.02);
 }
 
