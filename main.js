cc6c3a4fb580c2dc8e5a819eca60ce90a59317d3
cykim8811 <cykim8811@users.noreply.github.com>
2020-06-26 16:30:08 +0900

    hi

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 // Adding Light
 var light = new THREE.AmbientLight(0xffffff, 0.0);
@@ -13,35 +13,26 @@ var dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
 dirLight.position.set(0, 0, 0);
 dirLight.target.position.set(0, -10, 10);
 dirLight.castShadow = true;
-dirLight.shadow.mapSize.width = 4096;
-dirLight.shadow.mapSize.height = 4096;
-
-dirLight.shadow.camera.left = -100;
-dirLight.shadow.camera.right = 100;
-dirLight.shadow.camera.top = 100;
-dirLight.shadow.camera.bottom = -100;
 
 scene.add(dirLight);
 scene.add(dirLight.target);
 
-
-
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var geometry = new THREE.BoxGeometry(1, 1, 1);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
 
 function update() {
     cube.rotation.x += 0.005;
-    cube.rotation.y += 0.005;
+    cube.rotation.z += 0.005;
     setTimeout(update, 0.02);
 }
 
 function render() {
-    requestAnimationFrame( render );
-    renderer.render( scene, camera );
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
 }
 
 render();
