cf235121928e10e8fc68b58f26a8938a35135a32
cykim8811 <cykim8811@users.noreply.github.com>
2020-06-26 16:26:20 +0900

    DirLight change

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ scene.add(light);
 
 var dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
 dirLight.position.set(0, 0, 0);
+dirLight.target.position.set(0, -10, 10);
 dirLight.castShadow = true;
 dirLight.shadow.mapSize.width = 4096;
 dirLight.shadow.mapSize.height = 4096;
@@ -34,6 +35,7 @@ camera.position.z = 5;
 
 function update() {
     cube.rotation.x += 0.005;
+    cube.rotation.y += 0.005;
     setTimeout(update, 0.02);
 }
 
